23ab15b4eb6f1ed3be881417a917dc61fb370004
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-01-10 22:21:07 +0300

    ADD business and access layers for user

---
+++ DVLD_Business/User.cs
@@ -0,0 +1,93 @@
+﻿using DVLD_DataAccess;
+using System.Data;
+
+namespace DVLD_Business
+{
+    public class User
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+        public User()
+        {
+            this.Id = -1;
+            this.PersonId = -1;
+            this.Username = string.Empty;
+            this.Password = string.Empty;
+            this.IsActive = false;
+
+            _mode = Mode.Add;
+        }
+        private User(int Id, int PersonId, string Username, string Password, bool IsActive)
+        {
+            this.Id = Id;
+            this.PersonId = PersonId;
+            this.Username = Username;
+            this.Password = Password;
+            this.IsActive = IsActive;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id = UserData.Add(this.PersonId, this.Username, this.Password, this.IsActive);
+            return (this.Id != -1);
+        }
+
+        private bool _Update()
+        {
+            return UserData.Update(this.Id, this.PersonId, this.Username, this.Password, this.IsActive);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        _mode = Mode.Update;
+                        return _Add();
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int Id)
+        {
+            return UserData.Exist(Id);
+        }
+        public static bool Delete(int Id)
+        {
+            if (!Exist(Id))
+            {
+                return false;
+            }
+            else { return UserData.Delete(Id); }
+        }
+        public static DataTable All()
+        {
+            return UserData.All();
+        }
+        public static User Find(int Id)
+        {
+            int PersonId = -1;
+            string Username = string.Empty;
+            string Password = string.Empty;
+            bool IsActive = false;
+
+            if (UserData.Get(Id, ref PersonId, ref Username, ref Password, ref IsActive))
+            {
+                return new User(Id, PersonId, Username, Password, IsActive);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ DVLD_DataAccess/UserData.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public class UserData
+    {
+        public static int Add(int PersonId, string Username, string Password, bool IsActive)
+        {
+            int newId = 0;
+            string query = "insert into Users (PersonUsername,Password,IsActive) values (@PersonId,@Username,@Password,@IsActive) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    command.Parameters.AddWithValue("@Username", Username);
+                    command.Parameters.AddWithValue("@Password", Password);
+                    command.Parameters.AddWithValue("@IsActive", IsActive);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int Id, int PersonId, string Username, string Password, bool IsActive)
+        {
+            int RowsAffected = 0;
+            string query = "update Users set Id = @Id,PersonId = @PersonId,Username = @Username,Password = @Password,IsActive = @IsActive  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    command.Parameters.AddWithValue("@Username", Username);
+                    command.Parameters.AddWithValue("@Password", Password);
+                    command.Parameters.AddWithValue("@IsActive", IsActive);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int Id, ref int PersonId, ref string Username, ref string Password, ref bool IsActive)
+        {
+            bool IsFound = false;
+            string query = "select * from Users  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            PersonId = (int)reader["PersonId"];
+                            Username = (string)reader["Username"];
+                            Password = (string)reader["Password"];
+                            IsActive = (bool)reader["IsActive"];
+
+
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return IsFound;
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Users");
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("delete Users where Id = @Id", "@Id", Id);
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Users where Id= @Id", "@Id", Id);
+        }
+
+
+
+
+
+    }
+}
