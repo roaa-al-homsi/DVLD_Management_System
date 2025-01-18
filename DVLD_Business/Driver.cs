ca2221b8bd9b22d999086b6860f202a1a0295df9
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-01-18 21:07:17 +0300

    ADD business and access layers for Drivers

---
+++ DVLD_Business/Driver.cs
@@ -0,0 +1,89 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Data;
+
+namespace DVLD_Business
+{
+    public class Driver
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public int CreatedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Driver()
+        {
+            this.Id = -1;
+            this.PersonId = -1;
+            this.CreatedByUserId = -1;
+            this.CreatedDate = DateTime.MinValue;
+
+            _mode = Mode.Add;
+        }
+        private Driver(int Id, int PersonId, int CreatedByUserId, DateTime CreatedDate)
+        {
+            this.Id = Id;
+            this.PersonId = PersonId;
+            this.CreatedByUserId = CreatedByUserId;
+            this.CreatedDate = CreatedDate;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id =
+                        DriverData.Add(this.PersonId, this.CreatedByUserId, this.CreatedDate);
+            return (this.Id != -1);
+        }
+        private bool _Update()
+        {
+            return DriverData.Update(this.Id, this.PersonId, this.CreatedByUserId, this.CreatedDate);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        _mode = Mode.Update;
+                        return _Add();
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int Id)
+        {
+            return DriverData.Exist(Id);
+        }
+        public static bool Delete(int Id)
+        {
+            if (!Exist(Id))
+            {
+                return false;
+            }
+            else { return DriverData.Delete(Id); }
+        }
+        public static DataTable All()
+        {
+            return DriverData.All();
+        }
+        public static Driver Find(int Id)
+        {
+            int PersonId = -1;
+            int CreatedByUserId = -1;
+            DateTime CreatedDate = DateTime.MinValue;
+
+            if (DriverData.Get(Id, ref PersonId, ref CreatedByUserId, ref CreatedDate))
+            {
+                return new Driver(Id, PersonId, CreatedByUserId, CreatedDate);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ DVLD_DataAccess/DriverData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    static public class DriverData
+    {
+        public static int Add(int PersonId, int CreatedByUserId, DateTime CreatedDate)
+        {
+            int newId = 0;
+            string query = "insert into Drivers (PersonCreatedByUserCreatedDate) values (@PersonId,@CreatedByUserId,@CreatedDate) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+                    command.Parameters.AddWithValue("@CreatedDate", CreatedDate);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int Id, int PersonId, int CreatedByUserId, DateTime CreatedDate)
+        {
+            int RowsAffected = 0;
+            string query = "update Drivers set Id = @Id,PersonId = @PersonId,CreatedByUserId = @CreatedByUserId,CreatedDate = @CreatedDate  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@PersonId", PersonId);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+                    command.Parameters.AddWithValue("@CreatedDate", CreatedDate);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int Id, ref int PersonId, ref int CreatedByUserId, ref DateTime CreatedDate)
+        {
+            bool IsFound = false;
+            string query = "select * from Drivers  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            PersonId = (int)reader["PersonId"];
+                            CreatedByUserId = (int)reader["CreatedByUserId"];
+                            CreatedDate = (DateTime)reader["CreatedDate"];
+
+
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return IsFound;
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Drivers");
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("delete Drivers where Id = @Id", "@Id", Id);
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Drivers where Id= @Id", "@Id", Id);
+        }
+
+    }
+}
