7ce83baa4ce59fa2657428e15755c21b16e5d71a
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-01-18 21:05:06 +0300

    ADD business and access layers for License

---
+++ DVLD_Business/License.cs
@@ -0,0 +1,119 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Data;
+
+namespace DVLD_Business
+{
+    public class License
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int ApplicationId { get; set; }
+        public int DriverId { get; set; }
+        public int LicenseClassId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Notes { get; set; }
+        public decimal PaidFees { get; set; }
+        public bool IsActive { get; set; }
+        public byte IssueReason { get; set; }
+        public int CreatedByUserId { get; set; }
+
+        public License()
+        {
+            this.Id = -1;
+            this.ApplicationId = -1;
+            this.DriverId = -1;
+            this.LicenseClassId = -1;
+            this.IssueDate = DateTime.MinValue;
+            this.ExpirationDate = DateTime.MinValue;
+            this.Notes = string.Empty;
+            this.PaidFees = 0;
+            this.IsActive = false;
+            this.IssueReason = 0;
+            this.CreatedByUserId = -1;
+
+            _mode = Mode.Add;
+        }
+        private License(int Id, int ApplicationId, int DriverId, int LicenseClassId, DateTime IssueDate, DateTime ExpirationDate, string Notes, decimal PaidFees, bool IsActive, byte IssueReason, int CreatedByUserId)
+        {
+            this.Id = Id;
+            this.ApplicationId = ApplicationId;
+            this.DriverId = DriverId;
+            this.LicenseClassId = LicenseClassId;
+            this.IssueDate = IssueDate;
+            this.ExpirationDate = ExpirationDate;
+            this.Notes = Notes;
+            this.PaidFees = PaidFees;
+            this.IsActive = IsActive;
+            this.IssueReason = IssueReason;
+            this.CreatedByUserId = CreatedByUserId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id =
+                        LicenseData.Add(this.ApplicationId, this.DriverId, this.LicenseClassId, this.IssueDate, this.ExpirationDate, this.Notes, this.PaidFees, this.IsActive, this.IssueReason, this.CreatedByUserId);
+            return (this.Id != -1);
+        }
+
+        private bool _Update()
+        {
+            return LicenseData.Update(this.Id, this.ApplicationId, this.DriverId, this.LicenseClassId, this.IssueDate, this.ExpirationDate, this.Notes, this.PaidFees, this.IsActive, this.IssueReason, this.CreatedByUserId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        _mode = Mode.Update;
+                        return _Add();
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int Id)
+        {
+            return LicenseData.Exist(Id);
+        }
+        public static bool Delete(int Id)
+        {
+            if (!Exist(Id))
+            {
+                return false;
+            }
+            else { return LicenseData.Delete(Id); }
+        }
+        public static DataTable All()
+        {
+            return LicenseData.All();
+        }
+        public static License Find(int Id)
+        {
+            int ApplicationId = -1;
+            int DriverId = -1;
+            int LicenseClassId = -1;
+            DateTime IssueDate = DateTime.MinValue;
+            DateTime ExpirationDate = DateTime.MinValue;
+            string Notes = string.Empty;
+            decimal PaidFees = 0;
+            bool IsActive = false;
+            byte IssueReason = 0;
+            int CreatedByUserId = -1;
+
+            if (LicenseData.Get(Id, ref ApplicationId, ref DriverId, ref LicenseClassId, ref IssueDate, ref ExpirationDate, ref Notes, ref PaidFees, ref IsActive, ref IssueReason, ref CreatedByUserId))
+            {
+                return new License(Id, ApplicationId, DriverId, LicenseClassId, IssueDate, ExpirationDate, Notes, PaidFees, IsActive, IssueReason, CreatedByUserId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ DVLD_DataAccess/LicenseData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public static class LicenseData
+    {
+        public static int Add(int ApplicationId, int DriverId, int LicenseClassId, DateTime IssueDate, DateTime ExpirationDate, string Notes, decimal PaidFees, bool IsActive, byte IssueReason, int CreatedByUserId)
+        {
+            int newId = 0;
+            string query = "insert into Licenses (ApplicationDriverLicenseClassIssueDate,ExpirationDate,Notes,PaidFees,IsActive,IssueReason,CreatedByUserId) values (@ApplicationId,@DriverId,@LicenseClassId,@IssueDate,@ExpirationDate,@Notes,@PaidFees,@IsActive,@IssueReason,@CreatedByUserId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@ApplicationId", ApplicationId);
+                    command.Parameters.AddWithValue("@DriverId", DriverId);
+                    command.Parameters.AddWithValue("@LicenseClassId", LicenseClassId);
+                    command.Parameters.AddWithValue("@IssueDate", IssueDate);
+                    command.Parameters.AddWithValue("@ExpirationDate", ExpirationDate);
+                    command.Parameters.AddWithValue("@Notes", Notes);
+                    command.Parameters.AddWithValue("@PaidFees", PaidFees);
+                    command.Parameters.AddWithValue("@IsActive", IsActive);
+                    command.Parameters.AddWithValue("@IssueReason", IssueReason);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int Id, int ApplicationId, int DriverId, int LicenseClassId, DateTime IssueDate, DateTime ExpirationDate, string Notes, decimal PaidFees, bool IsActive, byte IssueReason, int CreatedByUserId)
+        {
+            int RowsAffected = 0;
+            string query = "update Licenses set Id = @Id,ApplicationId = @ApplicationId,DriverId = @DriverId,LicenseClassId = @LicenseClassId,IssueDate = @IssueDate,ExpirationDate = @ExpirationDate,Notes = @Notes,PaidFees = @PaidFees,IsActive = @IsActive,IssueReason = @IssueReason,CreatedByUserId = @CreatedByUserId  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@ApplicationId", ApplicationId);
+                    command.Parameters.AddWithValue("@DriverId", DriverId);
+                    command.Parameters.AddWithValue("@LicenseClassId", LicenseClassId);
+                    command.Parameters.AddWithValue("@IssueDate", IssueDate);
+                    command.Parameters.AddWithValue("@ExpirationDate", ExpirationDate);
+                    command.Parameters.AddWithValue("@Notes", Notes);
+                    command.Parameters.AddWithValue("@PaidFees", PaidFees);
+                    command.Parameters.AddWithValue("@IsActive", IsActive);
+                    command.Parameters.AddWithValue("@IssueReason", IssueReason);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int Id, ref int ApplicationId, ref int DriverId, ref int LicenseClassId, ref DateTime IssueDate, ref DateTime ExpirationDate, ref string Notes, ref decimal PaidFees, ref bool IsActive, ref byte IssueReason, ref int CreatedByUserId)
+        {
+            bool IsFound = false;
+            string query = "select * from Licenses  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            ApplicationId = (int)reader["ApplicationId"];
+                            DriverId = (int)reader["DriverId"];
+                            LicenseClassId = (int)reader["LicenseClassId"];
+                            IssueDate = (DateTime)reader["IssueDate"];
+                            ExpirationDate = (DateTime)reader["ExpirationDate"];
+                            Notes = (string)reader["Notes"];
+                            PaidFees = (decimal)reader["PaidFees"];
+                            IsActive = (bool)reader["IsActive"];
+                            IssueReason = (byte)reader["IssueReason"];
+                            CreatedByUserId = (int)reader["CreatedByUserId"];
+
+
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return IsFound;
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Licenses");
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("delete Licenses where Id = @Id", "@Id", Id);
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Licenses where Id= @Id", "@Id", Id);
+        }
+
+    }
+}
