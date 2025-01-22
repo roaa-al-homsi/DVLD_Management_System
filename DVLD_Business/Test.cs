f4258ed161ad7e4ce55f09393ef0c22ac793e7ad
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-01-23 01:35:42 +0300

    ADD Business and Access layers for Tests

---
+++ DVLD_Business/Test.cs
@@ -0,0 +1,94 @@
+﻿using DVLD_DataAccess;
+using System.Data;
+
+namespace DVLD_Business
+{
+    public class Test
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int TestAppointmentId { get; set; }
+        public bool Result { get; set; }
+        public string Notes { get; set; }
+        public int CreatedByUserId { get; set; }
+
+        public Test()
+        {
+            this.Id = -1;
+            this.TestAppointmentId = -1;
+            this.Result = false;
+            this.Notes = string.Empty;
+            this.CreatedByUserId = -1;
+
+            _mode = Mode.Add;
+        }
+        private Test(int Id, int TestAppointmentId, bool Result, string Notes, int CreatedByUserId)
+        {
+            this.Id = Id;
+            this.TestAppointmentId = TestAppointmentId;
+            this.Result = Result;
+            this.Notes = Notes;
+            this.CreatedByUserId = CreatedByUserId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id =
+                        TestData.Add(this.TestAppointmentId, this.Result, this.Notes, this.CreatedByUserId);
+            return (this.Id != -1);
+        }
+
+        private bool _Update()
+        {
+            return TestData.Update(this.Id, this.TestAppointmentId, this.Result, this.Notes, this.CreatedByUserId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        _mode = Mode.Update;
+                        return _Add();
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int Id)
+        {
+            return TestData.Exist(Id);
+        }
+        public static bool Delete(int Id)
+        {
+            if (!Exist(Id))
+            {
+                return false;
+            }
+            else { return TestData.Delete(Id); }
+        }
+        public static DataTable All()
+        {
+            return TestData.All();
+        }
+        public static Test Find(int Id)
+        {
+            int TestAppointmentId = -1;
+            bool Result = false;
+            string Notes = string.Empty;
+            int CreatedByUserId = -1;
+
+            if (TestData.Get(Id, ref TestAppointmentId, ref Result, ref Notes, ref CreatedByUserId))
+            {
+                return new Test(Id, TestAppointmentId, Result, Notes, CreatedByUserId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ DVLD_DataAccess/TestData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public static class TestData
+    {
+        public static int Add(int TestAppointmentId, bool Result, string Notes, int CreatedByUserId)
+        {
+            int newId = 0;
+            string query = "insert into Tests (TestAppointmentResult,Notes,CreatedByUserId) values (@TestAppointmentId,@Result,@Notes,@CreatedByUserId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@TestAppointmentId", TestAppointmentId);
+                    command.Parameters.AddWithValue("@Result", Result);
+                    command.Parameters.AddWithValue("@Notes", Notes);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int Id, int TestAppointmentId, bool Result, string Notes, int CreatedByUserId)
+        {
+            int RowsAffected = 0;
+            string query = "update Tests set TestAppointmentId = @TestAppointmentId,Result = @Result,Notes = @Notes,CreatedByUserId = @CreatedByUserId  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@TestAppointmentId", TestAppointmentId);
+                    command.Parameters.AddWithValue("@Result", Result);
+                    command.Parameters.AddWithValue("@Notes", Notes);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int Id, ref int TestAppointmentId, ref bool Result, ref string Notes, ref int CreatedByUserId)
+        {
+            bool IsFound = false;
+            string query = "select * from Tests  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            TestAppointmentId = (int)reader["TestAppointmentId"];
+                            Result = (bool)reader["Result"];
+                            Notes = (string)reader["Notes"];
+                            CreatedByUserId = (int)reader["CreatedByUserId"];
+
+
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return IsFound;
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Tests");
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("delete Tests where Id = @Id", "@Id", Id);
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from Tests where Id= @Id", "@Id", Id);
+        }
+
+    }
+}
