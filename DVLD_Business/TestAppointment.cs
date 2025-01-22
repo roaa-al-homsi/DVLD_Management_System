f61fad52a419a7c4ac1e4a605f6a93692edd13f3
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-01-23 01:33:38 +0300

    ADD Business and Access layers for TestAppointment

---
+++ DVLD_Business/TestAppointment.cs
@@ -0,0 +1,107 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Data;
+
+namespace DVLD_Business
+{
+    public class TestAppointment
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int Id { get; set; }
+        public int TestTypeId { get; set; }
+        public int LocalDrivingLicenseApplicationsId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal PaidFees { get; set; }
+        public int CreatedByUserId { get; set; }
+        public bool IsLocked { get; set; }
+        public int RetakeTestApplicationId { get; set; }
+
+        public TestAppointment()
+        {
+            this.Id = -1;
+            this.TestTypeId = -1;
+            this.LocalDrivingLicenseApplicationsId = -1;
+            this.Date = DateTime.MinValue;
+            this.PaidFees = 0;
+            this.CreatedByUserId = -1;
+            this.IsLocked = false;
+            this.RetakeTestApplicationId = -1;
+
+            _mode = Mode.Add;
+        }
+        private TestAppointment(int Id, int TestTypeId, int LocalDrivingLicenseApplicationsId, DateTime Date, decimal PaidFees, int CreatedByUserId, bool IsLocked, int RetakeTestApplicationId)
+        {
+            this.Id = Id;
+            this.TestTypeId = TestTypeId;
+            this.LocalDrivingLicenseApplicationsId = LocalDrivingLicenseApplicationsId;
+            this.Date = Date;
+            this.PaidFees = PaidFees;
+            this.CreatedByUserId = CreatedByUserId;
+            this.IsLocked = IsLocked;
+            this.RetakeTestApplicationId = RetakeTestApplicationId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.Id =
+                        TestAppointmentData.Add(this.TestTypeId, this.LocalDrivingLicenseApplicationsId, this.Date, this.PaidFees, this.CreatedByUserId, this.IsLocked, this.RetakeTestApplicationId);
+            return (this.Id != -1);
+        }
+
+        private bool _Update()
+        {
+            return TestAppointmentData.Update(this.Id, this.TestTypeId, this.LocalDrivingLicenseApplicationsId, this.Date, this.PaidFees, this.CreatedByUserId, this.IsLocked, this.RetakeTestApplicationId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        _mode = Mode.Update;
+                        return _Add();
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int Id)
+        {
+            return TestAppointmentData.Exist(Id);
+        }
+        public static bool Delete(int Id)
+        {
+            if (!Exist(Id))
+            {
+                return false;
+            }
+            else { return TestAppointmentData.Delete(Id); }
+        }
+        public static DataTable All()
+        {
+            return TestAppointmentData.All();
+        }
+        public static TestAppointment Find(int Id)
+        {
+            int TestTypeId = -1;
+            int LocalDrivingLicenseApplicationsId = -1;
+            DateTime Date = DateTime.MinValue;
+            decimal PaidFees = 0;
+            int CreatedByUserId = -1;
+            bool IsLocked = false;
+            int RetakeTestApplicationId = -1;
+
+            if (TestAppointmentData.Get(Id, ref TestTypeId, ref LocalDrivingLicenseApplicationsId, ref Date, ref PaidFees, ref CreatedByUserId, ref IsLocked, ref RetakeTestApplicationId))
+            {
+                return new TestAppointment(Id, TestTypeId, LocalDrivingLicenseApplicationsId, Date, PaidFees, CreatedByUserId, IsLocked, RetakeTestApplicationId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ DVLD_DataAccess/TestAppointmentData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public static class TestAppointmentData
+    {
+        public static int Add(int TestTypeId, int LocalDrivingLicenseApplicationsId, DateTime Date, decimal PaidFees, int CreatedByUserId, bool IsLocked, int RetakeTestApplicationId)
+        {
+            int newId = 0;
+            string query = "insert into TestAppointments (TestTypeLocalDrivingLicenseApplicationsDate,PaidFees,CreatedByUserIsLocked,RetakeTestApplicationId) values (@TestTypeId,@LocalDrivingLicenseApplicationsId,@Date,@PaidFees,@CreatedByUserId,@IsLocked,@RetakeTestApplicationId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@TestTypeId", TestTypeId);
+                    command.Parameters.AddWithValue("@LocalDrivingLicenseApplicationsId", LocalDrivingLicenseApplicationsId);
+                    command.Parameters.AddWithValue("@Date", Date);
+                    command.Parameters.AddWithValue("@PaidFees", PaidFees);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+                    command.Parameters.AddWithValue("@IsLocked", IsLocked);
+                    command.Parameters.AddWithValue("@RetakeTestApplicationId", (RetakeTestApplicationId == -1) ? DBNull.Value : (object)RetakeTestApplicationId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int Id, int TestTypeId, int LocalDrivingLicenseApplicationsId, DateTime Date, decimal PaidFees, int CreatedByUserId, bool IsLocked, int RetakeTestApplicationId)
+        {
+            int RowsAffected = 0;
+            string query = "update TestAppointments set TestTypeId = @TestTypeId,LocalDrivingLicenseApplicationsId = @LocalDrivingLicenseApplicationsId,Date = @Date,PaidFees = @PaidFees,CreatedByUserId = @CreatedByUserId,IsLocked = @IsLocked,RetakeTestApplicationId = @RetakeTestApplicationId  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Parameters.AddWithValue("@TestTypeId", TestTypeId);
+                    command.Parameters.AddWithValue("@LocalDrivingLicenseApplicationsId", LocalDrivingLicenseApplicationsId);
+                    command.Parameters.AddWithValue("@Date", Date);
+                    command.Parameters.AddWithValue("@PaidFees", PaidFees);
+                    command.Parameters.AddWithValue("@CreatedByUserId", CreatedByUserId);
+                    command.Parameters.AddWithValue("@IsLocked", IsLocked);
+                    command.Parameters.AddWithValue("@RetakeTestApplicationId", (RetakeTestApplicationId == -1) ? DBNull.Value : (object)RetakeTestApplicationId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int Id, ref int TestTypeId, ref int LocalDrivingLicenseApplicationsId, ref DateTime Date, ref decimal PaidFees, ref int CreatedByUserId, ref bool IsLocked, ref int RetakeTestApplicationId)
+        {
+            bool IsFound = false;
+            string query = "select * from TestAppointments  WHERE Id=@Id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", Id);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            IsFound = true;
+                            Id = (int)reader["Id"];
+                            TestTypeId = (int)reader["TestTypeId"];
+                            LocalDrivingLicenseApplicationsId = (int)reader["LocalDrivingLicenseApplicationsId"];
+                            Date = (DateTime)reader["Date"];
+                            PaidFees = (decimal)reader["PaidFees"];
+                            CreatedByUserId = (int)reader["CreatedByUserId"];
+                            IsLocked = (bool)reader["IsLocked"];
+                            RetakeTestApplicationId = reader["RetakeTestApplicationId"] != DBNull.Value ? (int)reader["RetakeTestApplicationId"] : -1;
+
+
+                        }
+                        else
+                        {
+                            IsFound = false;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return IsFound;
+        }
+        static public DataTable All()
+        {
+            return GenericData.All("select * from TestAppointments");
+        }
+        static public bool Delete(int Id)
+        {
+            return GenericData.Delete("delete TestAppointments where Id = @Id", "@Id", Id);
+        }
+        static public bool Exist(int Id)
+        {
+            return GenericData.Exist("select Found=1 from TestAppointments where Id= @Id", "@Id", Id);
+        }
+
+    }
+}
